2d58623720ea78c72ed13ae7aa6a837eb0c555a8
Hugo Mouto <hmoutol@gmail.com>
2021-06-07 11:37:49 -0300

    feat: /Load More/ button. Not implemmented yet

--- src/EpisodesList.jsx
+++ src/EpisodesList.jsx
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import EpisodeCard from './EpisodeCard';
 
 export default class EpisodesList extends Component {
-  toggleLinksContainer(index){
-    console.log(index)
-    if (index === this.state.displayLinksContainer) {
-      return `links-container--is-active`
-    } else {
-      return `links-container`
-    }
+  constructor() {
+    super()
   }
-  
+  wipFunction(e) {
+    e.preventDefault()
+    alert(`Obrigado pro testar o MVP.
+Essa funcionalidade ainda está em desenvolvimento.`)
+  }
   render() {
     const {podcastData} = this.props
     return (
       <ol className='episodes-list'>
         {podcastData.episodes.map((episode) => <EpisodeCard episode={episode} key={episode.id} />)}
-        <li className="filling-empty-space-childs"></li>
+        <li className="filling-empty-space-childs" onClick={this.wipFunction}>
+          <p className='btn-class btn-gradient load-more btn-float--hover'>carregar mais</p>
+        </li>
         <li className="filling-empty-space-childs"></li>
         <li className="filling-empty-space-childs"></li>
         <li className="filling-empty-space-childs"></li>
